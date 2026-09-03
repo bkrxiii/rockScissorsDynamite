232694f5156edc917de3374935efaacd877186be
Favio Becker <faviobecker@gmail.com>
2026-09-03 21:28:56 +0100

    c-version

--- README.md
+++
@@ -1,3 +0,0 @@
-# rockScissorsDynamite
-
-- Little terminal game of Rock, Scissors, Dynamite _(because paper never made sense)_.
--- src/RockScissorsDynamite.java
+++
@@ -1,7 +0,0 @@
-public class RockScissorsDynamite {
-    // keep score in here
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.game();
-    }
-}
